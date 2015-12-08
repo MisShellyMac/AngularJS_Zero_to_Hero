1d93754fcd0172d08b269e62d3972e174776ee13
MisShellyMac <michellemccarthy@hotmail.com>
2015-12-08 09:45:42 -0800

    removed commented out code that does not work

--- section1/lesson6/main.js
+++ section1/lesson6/main.js
@@ -2,25 +2,13 @@ var app = angular.module('minmax', [
 	'jcs-autoValidate'
 ]);
 
-/*
-angular.module('jcs-autoValidate').run([
-	'defaultErrorMessageResolver',
-	function (validator, defaultErrorMessageResolver) {
-	defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
-		errorMessages['tooYoung'] = 'You must be at least {0} years old to enter this site';
-		errorMessages['tooOld'] = 'You must not be older than {0} years old to enter this site';
-		errorMessages['badUsername'] = 'Username can only contain numbers and letters and _ ';
-	});
-}]);
-*/
-
 app.run( function(defaultErrorMessageResolver){
 	defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages){
 		errorMessages['tooYoung'] = 'You must be at least {0} years old to enter this site';
 		errorMessages['tooOld'] = 'You must not be older than {0} years old to enter this site';
 		errorMessages['badUsername'] = 'Username can only contain numbers and letters and _ ';
 	});
-} );
+});
 
 app.controller('MinMaxCtrl', function ($scope, $http) {
 	$scope.formModel = {};
